b4a597c18c36a85ce624881ef9496e5b7907156f
kodluyoruz <dilaracetinberk@gmail.com>
2023-04-18 19:43:15 +0300

    format

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   const [search, setSearch] = useState("");
   const [bookData, setData] = useState([]);
 
-  const handleSubmit = (e)=> {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBTqzA7y7GNB80yEBwq6wIDdvneutZus00')
-    .then(res=>setData(res.data.items))
-    .catch(err=>console.log(err))
-  }
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=" +
+          search +
+          "&key=AIzaSyBTqzA7y7GNB80yEBwq6wIDdvneutZus00"
+      )
+      .then((res) => setData(res.data.items))
+      .catch((err) => console.log(err));
+  };
 
-  const onChange = (e)=> {
-   setSearch(e.target.value)
-  }
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div className="App">
-      <Header handleSubmit={handleSubmit} onChange={onChange} search={search}/>
-      <div className='main-books'>
-				{bookData.map((book, index) => (
-						<Main
+      <Header handleSubmit={handleSubmit} onChange={onChange} search={search} />
+      <div className="main-books">
+        {bookData.map((book, index) => (
+          <Main
             key={index}
             imageLinks={book.volumeInfo.imageLinks?.thumbnail}
             title={book.volumeInfo?.title}
             authors={book.volumeInfo?.authors}
             description={book.volumeInfo?.description}
             pageCount={book.volumeInfo?.pageCount}
-            link= {book.volumeInfo?.previewLink}
-						/>
-					))}
-			</div>
-      <Footer />
+            link={book.volumeInfo?.previewLink}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,47 +1,53 @@
 import { useState } from "react";
-import { AiOutlineCloseCircle } from "react-icons/ai"
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Main({imageLinks, title, authors, description, pageCount, link }) {
+function Main({ imageLinks, title, authors, description, pageCount, link }) {
   const [showModal, setShowModal] = useState(false);
-
 
   return (
     <>
-    <div className="main">
-      <div className="main-img">
-        <img src={imageLinks} alt={title} />
+      <div className="main">
+        <div className="main-img">
+          <img src={imageLinks} alt={title} />
+        </div>
+
+        <div className="small-info">
+          <div className="title">{title}</div>
+          <div className="author">{authors}</div>
+          <button
+            type="button"
+            className="detail-btn"
+            onClick={() => setShowModal(true)}
+          >
+            Details
+          </button>
+        </div>
       </div>
 
-      <div className="small-info">
-        <div className="title">{title}</div>
-        <div className="author">{authors}</div>
-        <button type="button" className="detail-btn" onClick={() => setShowModal(true)}>Details</button>
-      </div>
-
-    </div>
-    
-    {showModal && (
-         <div className="overlay">
-         <div className="overlay-inner">
-             <button className="close" onClick={()=> setShowModal(false)}>
+      {showModal && (
+        <div className="overlay">
+          <div className="overlay-inner">
+            <button className="close" onClick={() => setShowModal(false)}>
               <AiOutlineCloseCircle />
-             </button>
-             <div className="inner-box">
-                 <img src={imageLinks} alt={title} />
-                 <div className="info">
-                     <h1>{title}</h1>
-                     <h3>{authors}</h3>
-                     <h4>{pageCount} pages</h4>
-                     
-                     <a href={link}><button>More</button></a>
-                 </div>
-             </div>
-             <h4 className="description">{description}</h4>
-         </div>
-     </div>
+            </button>
+            <div className="inner-box">
+              <img src={imageLinks} alt={title} />
+              <div className="info">
+                <h1>{title}</h1>
+                <h3>{authors}</h3>
+                <h4>{pageCount} pages</h4>
+
+                <a href={link}>
+                  <button>More</button>
+                </a>
+              </div>
+            </div>
+            <h4 className="description">{description}</h4>
+          </div>
+        </div>
       )}
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
