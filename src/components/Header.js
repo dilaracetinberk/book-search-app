41b71ad1004df1a054eff645ce820680bf83c717
kodluyoruz <dilaracetinberk@gmail.com>
2023-04-18 19:36:57 +0300

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-import React from 'react'
 
-function Header() {
+function Header({handleSubmit, onChange, search}) {
+ 
+
+
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <div className="header-content">
+        <h1>Book Search App</h1>
+        <form className="header-form"
+        onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Search Books"
+            value={search}
+            onChange={onChange}
+            required
+          />
+          <button type="submit" className="form-button">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import { useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai"
 
-function Main() {
+function Main({imageLinks, title, authors, description, pageCount, link }) {
+  const [showModal, setShowModal] = useState(false);
+
+
   return (
-    <div>Main</div>
+    <>
+    <div className="main">
+      <div className="main-img">
+        <img src={imageLinks} alt={title} />
+      </div>
+
+      <div className="small-info">
+        <div className="title">{title}</div>
+        <div className="author">{authors}</div>
+        <button type="button" className="detail-btn" onClick={() => setShowModal(true)}>Details</button>
+      </div>
+
+    </div>
+    
+    {showModal && (
+         <div className="overlay">
+         <div className="overlay-inner">
+             <button className="close" onClick={()=> setShowModal(false)}>
+              <AiOutlineCloseCircle />
+             </button>
+             <div className="inner-box">
+                 <img src={imageLinks} alt={title} />
+                 <div className="info">
+                     <h1>{title}</h1>
+                     <h3>{authors}</h3>
+                     
+                     <a href={link}><button>More</button></a>
+                 </div>
+             </div>
+             <h4 className="description">{description}</h4>
+         </div>
+     </div>
+      )}
+    </>
   )
 }
 
